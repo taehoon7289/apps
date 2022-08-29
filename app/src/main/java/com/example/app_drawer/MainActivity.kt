a309dd66755e1b200723b2a11d0c42e443c94c48
taehoon7289 <taehoon7289@gmail.com>
2022-08-29 18:43:24 +0900

    feat: view binding 실습
    - view binding 실습

--- app/src/main/java/com/example/app_drawer/MainActivity.kt
+++ app/src/main/java/com/example/app_drawer/MainActivity.kt
@@ -3,19 +3,45 @@ package com.example.app_drawer
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.example.app_drawer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding!!.root)
+
+        binding!!.mainText.text = "안녕!!"
 
         val packages = packageManager.getInstalledPackages(0)
         for (p in packages) {
             p.apply {
                 Log.d("package", "packageName $packageName")
+                val logoDrawable = packageManager.getApplicationLogo(packageName)
+                val iconDrawable = packageManager.getApplicationIcon(packageName)
+
+//                val imageViewForLogo: ImageView = findViewById<ImageView>(R.id.image_logo)
+//                imageViewForLogo.setImageDrawable(logoDrawable)
+//
+//                val imageViewForIcon: ImageView = findViewById<ImageView>(R.id.image_icon)
+//                imageViewForIcon.setImageDrawable(iconDrawable)
+
+                binding!!.imageLogo.setImageDrawable(logoDrawable)
+                binding!!.imageIcon.setImageDrawable(iconDrawable)
+
+
+
+                Log.d("package", "ddd ${packageManager.getApplicationIcon(packageName)}")
+
                 Log.d("package", "versionName $versionName")
                 Log.d("package", "lastUpdateTime $lastUpdateTime")
                 applicationInfo.apply {
@@ -31,6 +57,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        setContentView(R.layout.activity_main)
+
     }
 }
