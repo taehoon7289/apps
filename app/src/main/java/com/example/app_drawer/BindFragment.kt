921c9fd5178ee1525f2f42a3c0b5b55a7c1651da
taehoon7289 <taehoon7289@gmail.com>
2022-09-11 19:34:08 +0900

    feat: WebView loading lottie 추가
    - WebView loading lottie 추가

--- app/src/main/java/com/example/app_drawer/BindFragment.kt
+++ app/src/main/java/com/example/app_drawer/BindFragment.kt
@@ -9,7 +9,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Fragment() {
+abstract class BindFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Fragment() {
     lateinit var binding: T
 
     override fun onCreateView(
@@ -22,7 +22,7 @@ abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.lifecycleOwner = this@BaseFragment
+        binding.lifecycleOwner = this@BindFragment
         initView()
         super.onViewCreated(view, savedInstanceState)
     }
--- app/src/main/java/com/example/app_drawer/NotionWebViewActivity.kt
+++ app/src/main/java/com/example/app_drawer/NotionWebViewActivity.kt
@@ -1,25 +1,34 @@
 package com.example.app_drawer
 
-import android.os.Bundle
+import android.util.Log
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
 import com.example.app_drawer.databinding.ActivityNotionWebViewBinding
 
-class NotionWebViewActivity : AppCompatActivity() {
+class NotionWebViewActivity :
+    BindActivity<ActivityNotionWebViewBinding>(R.layout.activity_notion_web_view) {
 
-    private lateinit var activityNotionWebViewBinding: ActivityNotionWebViewBinding
     private lateinit var notionWewView: WebView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        activityNotionWebViewBinding = ActivityNotionWebViewBinding.inflate(layoutInflater)
-        setContentView(activityNotionWebViewBinding.root)
-    }
+    private val TAG = "NotionWebViewActivity"
 
     override fun onStart() {
         super.onStart()
-        notionWewView = activityNotionWebViewBinding.notionWebView
+        notionWewView = binding.notionWebView
+        notionWewView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                Log.d(TAG, "onProgressChanged: $newProgress")
+                super.onProgressChanged(view, newProgress)
+                if (newProgress > 90) {
+                    binding.notionWebViewAnimationView.isGone = true
+                    binding.notionWebView.isGone = false
+                } else {
+                    binding.notionWebViewAnimationView.isGone = false
+                    binding.notionWebView.isGone = true
+                }
+            }
+        }
 //        notionWewView.loadUrl("https://www.notion.so/4eee128c42924a89a885c1bef88ffacf")
 //        notionWewView.settings.loadWithOverviewMode = true
         notionWewView.settings.javaScriptEnabled = true
