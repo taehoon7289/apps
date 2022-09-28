5e0f0bbdbf120143d89b2cb3d4dc1f791f2d106c
taehoon7289 <taehoon7289@gmail.com>
2022-09-28 14:15:20 +0900

    feat: 테마 색상 지정
    - 테마 색상 지정

--- app/src/main/java/com/example/app_drawer/util/Util.kt
+++ app/src/main/java/com/example/app_drawer/util/Util.kt
@@ -2,6 +2,8 @@ package com.example.app_drawer.util
 
 import android.os.Build
 import android.util.Log
+import android.view.View
+import android.widget.ListView
 import androidx.annotation.RequiresApi
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -44,6 +46,28 @@ class Util {
         ): LocalDateTime? {
             return LocalDateTime.parse(str, DateTimeFormatter.ofPattern(pattern))
         }
+
+        fun setListViewHeightBasedOnChildren(listView: ListView) {
+            val listAdapter = listView.adapter
+            listAdapter?.let {
+                var totalHeight = 0
+                val desiredWidth =
+                    View.MeasureSpec.makeMeasureSpec(listView.width, View.MeasureSpec.AT_MOST)
+
+                for (i in 0 until listAdapter.count) {
+                    val listItem = listAdapter.getView(i, null, listView)
+                    listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED)
+                    totalHeight = totalHeight.plus(listItem.measuredHeight)
+                }
+
+                val params = listView.layoutParams
+                params.height =
+                    totalHeight.plus(listView.dividerHeight.times(listAdapter.count.minus(1)))
+                listView.layoutParams = params
+                listView.requestLayout()
+            }
+        }
+
 
         private const val TAG = "Util"
         private const val MAX_LEN = 2000
