3c8c3cb55c16a79c693367588160db76629fb460
taehoon7289 <taehoon7289@gmail.com>
2022-10-25 16:35:20 +0900

    feat: 알람예약시 분,시간 선택 추가
    - 알람예약시 분,시간 선택 추가

--- app/src/main/java/com/minikode/apps/ui/alarm/AlarmDialogFragment.kt
+++ app/src/main/java/com/minikode/apps/ui/alarm/AlarmDialogFragment.kt
@@ -1,15 +1,20 @@
 package com.minikode.apps.ui.alarm
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.RadioButton
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.minikode.apps.R
 import com.minikode.apps.code.AlarmPeriodType
 import com.minikode.apps.databinding.FragmentAlarmDialogBinding
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
@@ -45,6 +50,86 @@ class AlarmDialogFragment(
             val nowDate = Calendar.getInstance()
             this@AlarmDialogFragment.hourOfDay = nowDate.get(Calendar.HOUR_OF_DAY)
             this@AlarmDialogFragment.minute = nowDate.get(Calendar.MINUTE)
+
+            Log.d(TAG, "initView: hourOfDay $hourOfDay")
+            Log.d(TAG, "initView: minute $minute")
+
+            spinnerTimer.adapter = ArrayAdapter(
+                requireActivity(),
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                mutableListOf(
+                    "직접입력",
+                    "1분후",
+                    "2분후",
+                    "5분후",
+                    "10분후",
+                    "30분후",
+                    "1시간후",
+                    "2시간후",
+                    "5시간후",
+                    "10시간후",
+                )
+            )
+            spinnerTimer.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                @RequiresApi(Build.VERSION_CODES.M)
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    Log.d(TAG, "onItemSelected: $position $id")
+                    val nowTime = Calendar.getInstance()
+                    val nowHour = nowTime.get(Calendar.HOUR_OF_DAY)
+                    val nowMinute = nowTime.get(Calendar.MINUTE)
+                    when (position) {
+                        1 -> {
+                            timePicker.hour = nowHour
+                            timePicker.minute = nowMinute.plus(1)
+                        }
+                        2 -> {
+                            timePicker.hour = nowHour
+                            timePicker.minute = nowMinute.plus(2)
+                        }
+                        3 -> {
+                            timePicker.hour = nowHour
+                            timePicker.minute = nowMinute.plus(5)
+                        }
+                        4 -> {
+                            timePicker.hour = nowHour
+                            timePicker.minute = nowMinute.plus(10)
+                        }
+                        5 -> {
+                            timePicker.hour = nowHour
+                            timePicker.minute = nowMinute.plus(30)
+                        }
+                        6 -> {
+                            timePicker.hour = nowHour.plus(1)
+                            timePicker.minute = nowMinute
+                        }
+                        7 -> {
+                            timePicker.hour = nowHour.plus(2)
+                            timePicker.minute = nowMinute
+                        }
+                        8 -> {
+                            timePicker.hour = nowHour.plus(5)
+                            timePicker.minute = nowMinute
+                        }
+                        9 -> {
+                            timePicker.hour = nowHour.plus(10)
+                            timePicker.minute = nowMinute
+                        }
+                        else -> {
+                            timePicker.hour = nowHour
+                            timePicker.minute = nowMinute
+                        }
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    Log.d(TAG, "onNothingSelected: ")
+                }
+            }
             timePicker.setOnTimeChangedListener { _, hourOfDay, minute ->
                 this@AlarmDialogFragment.hourOfDay = hourOfDay
                 this@AlarmDialogFragment.minute = minute
@@ -82,5 +167,9 @@ class AlarmDialogFragment(
             }
         }
 
+    }
+
+    companion object {
+        private const val TAG = "AlarmDialogFragment"
     }
 }
